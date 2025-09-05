48cff1060d2a3bd7218ee409921c5970c3d55d6f
medhat <medhatmoris90@gmail.com>
2025-09-06 02:53:32 +0300

    fix: Set basename for BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import Shopprovider from './Context/Context.jsx'
+import ContextProvider from './Context/Context.jsx'
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-    <Shopprovider>
-         <App />
-    </Shopprovider>
-
-    </BrowserRouter>
-
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ContextProvider>
+      <BrowserRouter basename="/FOREVER/">
+        <App />
+      </BrowserRouter>
+    </ContextProvider>
+  </React.StrictMode>,
 )

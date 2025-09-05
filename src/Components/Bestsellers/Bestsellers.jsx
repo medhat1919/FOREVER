3c66bca705814e110c77bf0798d95ea2e2de8d23
medhat <medhatmoris90@gmail.com>
2025-09-06 02:37:30 +0300

    feat: Update project files

--- src/Components/Bestsellers/Bestsellers.jsx
+++ src/Components/Bestsellers/Bestsellers.jsx
@@ -9,7 +9,7 @@ const Bestsellers = () => {
     <div className='best-sellers'>
         <div className="text">
             <h1 className='text-4xl font-medium text-black mb-5 relative'> <span className='text-gray-400 text-4xl'>Best</span> Sellers</h1>
-            <p className='text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur suscipit sed earum adipisci repellat ab?</p>
+            <p className='text-gray-600'>this is bestsellers</p>
         </div>
         <div className="collections  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-5">
             {products.filter(product => product.bestseller === true).slice(0, 8).map((asset,_id) =>
